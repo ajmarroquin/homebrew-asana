65b7e21a295c0a70ed41d7ae4896656656693924
AJ Marroquin <43194782+ajmarroquin@users.noreply.github.com>
2021-03-24 21:15:16 -0400

    Update asana.rb
    changing codegangsta to rufav and thash to ajmarroquin

--- asana.rb
+++ asana.rb
@@ -2,16 +2,16 @@ require "formula"
 
 ASANA_VERSION = '0.2.1'
 class Asana < Formula
-  homepage 'https://github.com/thash/asana'
-  url 'https://github.com/thash/asana.git', tag: "v#{ASANA_VERSION}"
+  homepage 'https://github.com/ajmarroquin/asana'
+  url 'https://github.com/ajmarroquin/asana.git', tag: "v#{ASANA_VERSION}"
   version ASANA_VERSION
 
   depends_on 'go' => :build
 
   def install
     ENV['GOPATH'] = buildpath
-    system 'go', 'get', 'github.com/codegangsta/cli'
-    system 'go', 'get', 'github.com/thash/asana'
+    system 'go', 'get', 'github.com/urfav/cli'
+    system 'go', 'get', 'github.com/ajmarroquin/asana'
     system 'go', 'build', '-o', 'asana'
     bin.install 'asana'
   end
